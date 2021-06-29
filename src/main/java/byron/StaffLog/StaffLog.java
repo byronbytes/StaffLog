fb4c001ac3ab5b5fabc9907fd37a4d8e6b36b06d
Byron <53088136+byronbytes@users.noreply.github.com>
2021-06-28 20:04:01 -0400

    Update StaffLog.java

--- src/main/java/byron/StaffLog/StaffLog.java
+++ src/main/java/byron/StaffLog/StaffLog.java
@@ -10,18 +10,18 @@ public class StaffLog extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("StaffLog has been loaded.");
+        getLogger().info("[SL] StaffLog has been loaded.");
         this.getCommand("commandblock").setExecutor(new CommandBlock());
 
         for(Player p : Bukkit.getOnlinePlayers()){
             if(p.hasPermission("stafflog.debug")){
-                p.sendMessage("[SL] StaffLog has been loaded.");
+                p.sendMessage("[SL] StaffLog has been disabled.");
             }
         }
     }
     @Override
     public void onDisable() {
-        getLogger().info("StaffLog has unloaded.");
+        getLogger().info("[SL] StaffLog has unloaded.");
 
 
         for(Player p : Bukkit.getOnlinePlayers()){
